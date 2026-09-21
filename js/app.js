09c1c0dd3f4ea837ec6a1b02f93c534934960617
Val <60014136+voffee@users.noreply.github.com>
2026-09-21 08:31:03 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+
+function createRange(start, end) {
+    return Array.from(
+        { length: end - start + 1 },
+        (_, index) => index + start
+    );
+}
+
+function createRangeWithStep(start, end, step) {
+    return Array.from(
+        {length: (((end-start)/ 2) + 1)},
+        (_, index) => index * step
+    );
+}
+
+console.log('Test');
